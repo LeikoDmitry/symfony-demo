63c306c5279e648d4e311f1a26e4ebfa83a3e2b5
Leyko Dmitry <d.leyko@itransition.com>
2020-03-23 20:10:38 +0300

    Update Login Page

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,6 +8,7 @@ use Laminas\Authentication\AuthenticationServiceInterface;
 use Laminas\Form\Element\Csrf;
 use Laminas\Http\Response;
 use Laminas\Mvc\Controller\AbstractActionController;
+use Laminas\Session\SessionManager;
 use Laminas\View\Model\ViewModel;
 use Laminas\Form\Annotation\AnnotationBuilder;
 use SymfonyDemo\Entity\User;
@@ -37,6 +38,10 @@ class SecurityController extends AbstractActionController
      */
     public function loginAction()
     {
+        if ($this->identity()) {
+
+            return $this->redirect()->toRoute('blog');
+        }
         $builder = new AnnotationBuilder();
         $form = $builder->createForm(User::class);
         $form->add([
@@ -48,36 +53,49 @@ class SecurityController extends AbstractActionController
                 ],
             ],
         ]);
-        if (! $this->getRequest()->isPost()) {
+        $prg = $this->prg('login', true);
+        if ($prg instanceof Response) {
+
+            return $prg;
+        }
+        if ($prg === false) {
 
             return new ViewModel(['form' => $form]);
         }
-        $request = $this->getRequest();
-        $data = $request->getPost();
-        $form->setData($data);
+        $form->setData($prg);
         $form->setValidationGroup('username', 'password', '__csrf');
         if (! $form->isValid()) {
 
-            return $this->redirect()->toRoute('login');
+            return new ViewModel(['form' => $form]);
         }
         $adapter = $this->authenticationService->getAdapter();
-        $adapter->setIdentity($form->getData()['username']);
-        $adapter->setCredential($form->getData()['password']);
+        $adapter->setIdentity($form->getData()['username'] ?? '');
+        $adapter->setCredential($form->getData()['password'] ?? '');
         $authResult = $this->authenticationService->authenticate();
         if (! $authResult->isValid()) {
-            return $this->redirect()->toRoute('login');
+            $form->setMessages([
+                'username' => $authResult->getMessages()
+            ]);
+
+            return new ViewModel(['form' => $form]);
         }
         $identity = $authResult->getIdentity();
         $this->authenticationService->getStorage()->write($identity);
 
-        return $this->redirect()->toRoute('home');
+        return $this->redirect()->toRoute('blog');
     }
 
     /**
-     * @return ViewModel
+     * @return Response
      */
-    public function logoutAction()
+    public function logoutAction(): Response
     {
-        return new ViewModel();
+        if ($this->authenticationService->hasIdentity()) {
+            $this->authenticationService->clearIdentity();
+            $sessionManager = new SessionManager();
+            $sessionManager->forgetMe();
+        }
+
+        return $this->redirect()->toRoute('login');
     }
 }
